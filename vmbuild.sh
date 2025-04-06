63762f100857580178499ee4d995be3b24c65a41
Jan Kammerath <jankammerath@gmail.com>
2025-04-06 14:19:00 +0200

    base for linuxkit image config

--- vmbuild.sh
+++ vmbuild.sh
@@ -6,5 +6,11 @@ if [[ "$OSTYPE" != "darwin"* ]]; then
   exit 1
 fi
 
+# Check if LinuxKit is installed
+if ! command -v linuxkit >/dev/null 2>&1; then
+  echo "linuxkit is not installed. Please install it."
+  exit 1
+fi
+
 # Build for arm64 linux 
 GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -o ./bin/snippetd ./main.go
