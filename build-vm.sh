02934e35822ff0ad8f1148a43844a1a1732a0739
jankammerath <jankammerath@gmail.com>
2025-04-06 11:07:58 +0000

    feat: update build script to include arm64 linux build command

--- build-vm.sh
+++ build-vm.sh
@@ -5,3 +5,6 @@ if [[ "$OSTYPE" != "darwin"* ]]; then
   echo "This script is intended to be run on macOS."
   exit 1
 fi
+
+# Build for arm64 linux 
+GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -o ./bin/snippetd ./main.go
